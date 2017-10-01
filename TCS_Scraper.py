de1bbffad3bd09fceb92165e1e2619010d0f2201
Peter Yang <me@peteryang.io>
2017-09-30 17:01:22 -0700

    looking for team urls

--- TCS_Scraper.py
+++ TCS_Scraper.py
@@ -1,32 +1,13 @@
-from bs4 import BeautifulSoup
-
 import urllib.request
 
-def get_teams():
-    url = "https://compete.tespa.org/tournament/75/phase/1"
-    page = urllib.request.urlopen(url)
-    # "wb" because page.read() gives us bytes so we write bytes
-    with open("teams_page.html", "wb") as teams_page:
-        content = page.read()
-        teams_page.write(content)
+class TCS_Scraper():
+    @staticmethod
+    def get_teams():
+        url = "https://compete.tespa.org/tournament/75/phase/1"
+        page = urllib.request.urlopen(url)
+        # "wb" because page.read() gives us bytes so we write bytes
+        with open("teams_page.html", "wb") as teams_page:
+            content = page.read()
+            teams_page.write(content)
 
-def soup():
-    with open("teams_page.html", "rb") as teams_page:
-        soup = BeautifulSoup(teams_page.read(), "html.parser")
-
-    # Print page
-    # print("soup.prettify:", soup.prettify())
-
-    # Print all tags
-    print("\nEach tag in soup")
-    for tag in soup:
-        print(tag)
-
-    # Print table
-    tableTag = soup.table
-    print("\ntableTag:", tableTag)
-    print("\ntableTag.name:", tableTag.name)
-    tableAttributes = tableTag.attrs
-    print("\ntableAttributes:", tableAttributes)
-
-soup()
+TCS_Scraper.get_teams()
