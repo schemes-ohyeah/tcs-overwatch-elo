fadf3ec6543fb22d094106b485bdfa85035a79cc
spark0698 <sean91827364@gmail.com>
2017-09-30 20:22:51 -0400

    Overbuff scrape

---
+++ Overbuff_Scraper.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import requests
+requests.packages.urllib3.disable_warnings()
+
+class Overbuff_Scraper():
+    @staticmethod
+    def get_sr(battle_tag):
+        user, id = battle_tag.split("#")
+        url = "https://www.overbuff.com/players/pc/{0}-{1}".format(user, id)
+        print(url)
+        r = requests.get(url, timeout=20, verify=False)
+
+        raw_html = r.text
+        soup = BeautifulSoup(raw_html, "html.parser")
+
+        sr = soup.find("span", {"class": "color-stat-rating"})
+
+        return sr.text
